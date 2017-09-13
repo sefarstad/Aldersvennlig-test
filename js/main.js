022938ab92dc5731130cb49c555bd7ae7a77aa03
sefarstad <svein.farstad@makingwaves.com>
2017-09-13 20:02:36 +0200

    checkss

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $( document ).ready(function() {
+	$("#retur").hide();
     $("#test1").on('click', function(event) {
 	  $('html, body').animate({scrollTop:$(this).position().top - 60}, 'slow');
+		$("#retur").fadeIn();
 	});
 	/*$("#test2").on('click', function(event) {
 	  $('html, body').animate({scrollTop:$(this).position().top - 160}, 'slow');
