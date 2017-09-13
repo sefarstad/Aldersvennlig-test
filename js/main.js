ba1e7ef402e62994537e14d0c2be8d035ed6be0b
sefarstad <svein.farstad@makingwaves.com>
2017-09-13 19:26:07 +0200

    scrollfix

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-
+$( document ).ready(function() {
+    $("#test1").on('click', function(event) {
+	  $('html, body').animate({scrollTop:$(this).position().top - 160}, 'slow');
+	});
+	/*$("#test2").on('click', function(event) {
+	  $('html, body').animate({scrollTop:$(this).position().top - 160}, 'slow');
+	});
+	$("#test3").on('click', function(event) {
+	  $('html, body').animate({scrollTop:$(this).position().top - 160}, 'slow');
+	});*/
+	
+});
