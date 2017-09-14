01bfd8494d73f3830b19aef73b21a830bc845b6b
sefarstad <svein.farstad@makingwaves.com>
2017-09-14 09:11:19 +0200

    adresses

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $( document ).ready(function() {
 	$("#retur").hide();
+	$(".ad1").hide();
+	$(".ad2").hide();
     $("#test1").on('click', function(event) {
 	  $('html, body').animate({scrollTop:$(this).position().top - 60}, 'slow');
 	  if ($("#test1").is(':checked')) {
@@ -9,6 +11,24 @@ $( document ).ready(function() {
 			$("#retur").slideToggle();
 		}
 	});
+	$( "#ad1f" ).focus(function() { 
+		$(".ad1").slideToggle();
+	});
+	$( "#ad2f" ).focus(function() { 
+		$(".ad2").slideToggle();
+	});
+
+	$("#a1").on('click', function(event) {
+	 $("#ad1f").val("Sogn frivillighetssentral");
+	 $(".ad1").slideToggle();
+	});
+
+	$("#a2").on('click', function(event) {
+	 $("#ad2f").val("Storo Storsenter");
+	 $(".ad2").slideToggle();
+	});
+
+
 	/*$("#test2").on('click', function(event) {
 	  $('html, body').animate({scrollTop:$(this).position().top - 160}, 'slow');
 	});
