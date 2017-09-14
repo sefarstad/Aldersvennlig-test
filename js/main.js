06f07b45ca1c594cec2a7fbf22674d74044c6644
sefarstad <svein.farstad@makingwaves.com>
2017-09-14 08:58:45 +0200

    big

--- js/main.js
+++ js/main.js
@@ -2,7 +2,12 @@ $( document ).ready(function() {
 	$("#retur").hide();
     $("#test1").on('click', function(event) {
 	  $('html, body').animate({scrollTop:$(this).position().top - 60}, 'slow');
-		$("#retur").fadeIn();
+	  if ($("#test1").is(':checked')) {
+			$("#retur").slideToggle();
+		}
+		else {
+			$("#retur").slideToggle();
+		}
 	});
 	/*$("#test2").on('click', function(event) {
 	  $('html, body').animate({scrollTop:$(this).position().top - 160}, 'slow');
